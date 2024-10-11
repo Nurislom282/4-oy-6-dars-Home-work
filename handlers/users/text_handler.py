2ef8cd3e4e4517aa0b448d748950cc8ad7836a37
Nurislom <anurislom425@gmail.com>
2024-10-11 15:21:02 +0500

    Tayyor

--- handlers/users/text_handler.py
+++ handlers/users/text_handler.py
@@ -1,9 +1,19 @@
 from telebot.types import Message,ReplyKeyboardRemove
+from telebot.types import Message, CallbackQuery
+import requests
+import json
 
 from data import bot
 from buttons.default import phone_num
+from config import parameters
+
+
 
 USER_INFO = {}
+
+def write_to_json(user_info):
+    with open('Users_info.json',mode="a",encoding="utf-8") as file:
+        json.dump(user_info,file,indent=4)
 def validate__name(text):
     if len(text.split()) >= 2 and len(text.split()) <= 4:
         lowercase = "abcdefghijklmnopqrstuvwxyz'"
@@ -63,3 +73,27 @@ def get_phone_number(message: Message):
                                    reply_markup=phone_num())
             bot.register_next_step_handler(msg, get_full_name)
     del USER_INFO[from_user_id]
+
+@bot.callback_query_handler(func=lambda call: call.data == 'loc')
+def reaction_to_ob_havo(call: CallbackQuery):
+    chat_id = call.message.chat.id
+    bot.delete_message(chat_id, call.message.message_id)
+    bot.send_message(chat_id, "Locatsiyani yuboring")
+
+
+@bot.message_handler(content_types=['location'])
+def loc(message: Message):
+    chat_id = message.chat.id
+    long = message.location.longitude
+    lat = message.location.latitude
+    parameters['lat'] = lat
+    parameters['lon'] = long
+
+    data = requests.get("https://api.openweathermap.org/data/2.5/weather?", params=parameters).json()
+    temp = data["main"]["temp"]
+    wind = data["wind"]["speed"]
+    text = f"Ob havo ma'lumoti\nTemp: {temp}C\nWind: {wind}"
+
+    write_to_json(USER_INFO)
+
+    bot.send_message(chat_id, text)
